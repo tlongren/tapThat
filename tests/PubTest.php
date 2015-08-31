b92d84b8b4b054faab4df72faaea5b50bc464beb
Alexandra Brown & Deron Johnson <student@epicodus.com>
2015-08-31 14:13:16 -0700

    Add PubTest class with failing test for getName method

--- tests/PubTest.php
+++ tests/PubTest.php
@@ -1,4 +1,38 @@
 <?php
 
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
 
- ?>
+    require_once "src/Pub.php";
+
+    $server = "mysql:host=localhost;dbname=tap_that_test";
+    $username = "root";
+    $password = "root";
+    $DB = new PDO($server, $username, $password);
+
+    class PubTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            //Pub::deleteAll();
+        }
+
+        function test_getName()
+        {
+            //Arrange
+            $name = "Paddys";
+            $location = "462 Over There Way";
+            $link = "www.paddyspub.com";
+            $test_pub = new Pub($name, $location, $link);
+
+            //Act
+            $result = $test_pub->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+    }
+
+?>
