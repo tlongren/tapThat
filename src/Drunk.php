9ff8bee10ab4535ba3e47ba8bf13be59bddc1fec
calascionec <chris.calascione@gmail.com>
2015-08-31 13:27:57 -0700

    add getters and setters for drunk class and pass all tests

--- src/Drunk.php
+++ src/Drunk.php
@@ -2,8 +2,68 @@
 
     class Drunk
     {
+        private $name;
+        private $date_of_birth;
+        private $location;
+        private $email;
+        private $id;
 
-        
+
+        function __construct($name, $date_of_birth, $location, $email, $id = null)
+        {
+            $this->name = $name;
+            $this->date_of_birth = $date_of_birth;
+            $this->location = $location;
+            $this->email = $email;
+            $this->id = $id;
+        }
+
+        ////Getters and Setters
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function getDateOfBirth()
+        {
+            return $this->date_of_birth;
+        }
+
+        function setDateOfBirth($new_date_of_birth)
+        {
+            $this->date_of_birth = $new_date_of_birth;
+        }
+
+        function getLocation()
+        {
+            return $this->location;
+        }
+
+        function setLocation($new_location)
+        {
+            $this->location = $new_location;
+        }
+
+        function getEmail()
+        {
+            return $this->email;
+        }
+
+        function setEmail($new_email)
+        {
+            $this->email = $new_email;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
     }
 
  ?>
---
+++ tests/DrunkTest.php
@@ -0,0 +1,192 @@
+<?php
+
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Drunk.php";
+
+
+    $server = 'mysql:host=localhost:8889;dbname=tap_that_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class DrunkTest extends PHPUnit_Framework_TestCase
+    {
+
+        // protected function()
+        // {
+        //     Drunk::deleteAll();
+        // }
+
+        function testGetName()
+        {
+            //Arrange
+            $name = "Person 1";
+            $date_of_birth = "1988-03-04";
+            $location = "Portland, OR";
+            $email = "email@email.com";
+            $id = 1;
+            $test_drunk = new Drunk($name, $date_of_birth, $location, $email, $id);
+
+            //Act
+            $result = $test_drunk->getName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+
+        function testSetName()
+        {
+            //Arrange
+            $name = "Person 1";
+            $date_of_birth = "1988-03-04";
+            $location = "Portland, OR";
+            $email = "email@email.com";
+            $id = 1;
+            $test_drunk = new Drunk($name, $date_of_birth, $location, $email, $id);
+
+            $new_name = "Person 2";
+
+            //Act
+            $test_drunk->setName($new_name);
+            $result = $test_drunk->getName();
+
+            //Assert
+            $this->assertEquals($new_name, $result);
+        }
+
+        function testGetDateOfBirth()
+        {
+            //Arrange
+            $name = "Person 1";
+            $date_of_birth = "1988-03-04";
+            $location = "Portland, OR";
+            $email = "email@email.com";
+            $id = 1;
+            $test_drunk = new Drunk($name, $date_of_birth, $location, $email, $id);
+
+            //Act
+            $result = $test_drunk->getDateOfBirth();
+
+            //Assert
+            $this->assertEquals($date_of_birth, $result);
+        }
+
+        function testSetDateOfBirth()
+        {
+            //Arrange
+            $name = "Person 1";
+            $date_of_birth = "1988-03-04";
+            $location = "Portland, OR";
+            $email = "email@email.com";
+            $id = 1;
+            $test_drunk = new Drunk($name, $date_of_birth, $location, $email, $id);
+
+            $new_date_of_birth = "1987-08-09";
+
+            //Act
+            $test_drunk->setDateOfBirth($new_date_of_birth);
+            $result = $test_drunk->getDateOfBirth();
+
+            //Assert
+            $this->assertEquals($new_date_of_birth, $result);
+        }
+
+        function testGetLocation()
+        {
+            //Arrange
+            $name = "Person 1";
+            $date_of_birth = "1988-03-04";
+            $location = "Portland, OR";
+            $email = "email@email.com";
+            $id = 1;
+            $test_drunk = new Drunk($name, $date_of_birth, $location, $email, $id);
+
+            //Act
+            $result = $test_drunk->getLocation();
+
+            //Assert
+            $this->assertEquals($location, $result);
+        }
+
+        function testSetLocation()
+        {
+            //Arrange
+            $name = "Person 1";
+            $date_of_birth = "1988-03-04";
+            $location = "Portland, OR";
+            $email = "email@email.com";
+            $id = 1;
+            $test_drunk = new Drunk($name, $date_of_birth, $location, $email, $id);
+
+            $new_location = "Chapel HIll, NC";
+
+            //Act
+            $test_drunk->setLocation($new_location);
+            $result = $test_drunk->getLocation();
+
+            //Assert
+            $this->assertEquals($new_location, $result);
+        }
+
+        function testGetEmail()
+        {
+            //Arrange
+            $name = "Person 1";
+            $date_of_birth = "1988-03-04";
+            $location = "Portland, OR";
+            $email = "email@email.com";
+            $id = 1;
+            $test_drunk = new Drunk($name, $date_of_birth, $location, $email, $id);
+
+            //Act
+            $result = $test_drunk->getEmail();
+
+            //Assert
+            $this->assertEquals($email, $result);
+        }
+
+        function testSetEmail()
+        {
+            //Arrange
+            $name = "Person 1";
+            $date_of_birth = "1988-03-04";
+            $location = "Portland, OR";
+            $email = "email@email.com";
+            $id = 1;
+            $test_drunk = new Drunk($name, $date_of_birth, $location, $email, $id);
+
+            $new_email = "this@this.com";
+
+            //Act
+            $test_drunk->setEmail($new_email);
+            $result = $test_drunk->getEmail();
+
+            //Assert
+            $this->assertEquals($new_email, $result);
+        }
+
+        function testGetId()
+        {
+            //Arrange
+            $name = "Person 1";
+            $date_of_birth = "1988-03-04";
+            $location = "Portland, OR";
+            $email = "email@email.com";
+            $id = 1;
+            $test_drunk = new Drunk($name, $date_of_birth, $location, $email, $id);
+
+            //Act
+            $result = $test_drunk->getId();
+
+            //Assert
+            $this->assertEquals($id, $result);
+        }
+
+    }
+
+ ?>
