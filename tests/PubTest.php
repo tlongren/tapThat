781b6c93ad9529875ed07427fc0622ac07fe27a6
Alexandra Brown & Deron Johnson <student@epicodus.com>
2015-08-31 14:26:54 -0700

    Add test for getId in Pub class

--- tests/PubTest.php
+++ tests/PubTest.php
@@ -53,72 +53,88 @@
         }
 
 
-            function test_getLocation()
-            {
-                //Arrange
-                $name = "Paddys";
-                $location = "462 Over There Way";
-                $link = "www.paddyspub.com";
-                $test_pub = new Pub($name, $location, $link);
+        function test_getLocation()
+        {
+            //Arrange
+            $name = "Paddys";
+            $location = "462 Over There Way";
+            $link = "www.paddyspub.com";
+            $test_pub = new Pub($name, $location, $link);
 
-                //Act
-                $result = $test_pub->getLocation();
+            //Act
+            $result = $test_pub->getLocation();
 
-                //Assert
-                $this->assertEquals($location, $result);
-            }
+            //Assert
+            $this->assertEquals($location, $result);
+        }
 
-            function test_setLocation()
-            {
-                //Arrange
-                $name = "Paddys";
-                $location = "462 Over There Way";
-                $link = "www.paddyspub.com";
-                $test_pub = new Pub($name, $location, $link);
+        function test_setLocation()
+        {
+            //Arrange
+            $name = "Paddys";
+            $location = "462 Over There Way";
+            $link = "www.paddyspub.com";
+            $test_pub = new Pub($name, $location, $link);
 
-                $new_location = "234 Get There Place";
+            $new_location = "234 Get There Place";
 
-                //Act
-                $test_pub->setLocation($new_location);
-                $result = $test_pub->getLocation();
+            //Act
+            $test_pub->setLocation($new_location);
+            $result = $test_pub->getLocation();
 
-                //Assert
-                $this->assertEquals($new_location, $result);
-            }
+            //Assert
+            $this->assertEquals($new_location, $result);
+        }
 
 
-                function test_getLink()
-                {
-                    //Arrange
-                    $name = "Paddys";
-                    $location = "462 Over There Way";
-                    $link = "www.paddyspub.com";
-                    $test_pub = new Pub($name, $location, $link);
+        function test_getLink()
+        {
+            //Arrange
+            $name = "Paddys";
+            $location = "462 Over There Way";
+            $link = "www.paddyspub.com";
+            $test_pub = new Pub($name, $location, $link);
 
-                    //Act
-                    $result = $test_pub->getLink();
+            //Act
+            $result = $test_pub->getLink();
 
-                    //Assert
-                    $this->assertEquals($link, $result);
-                }
+            //Assert
+            $this->assertEquals($link, $result);
+        }
 
-                function test_setLink()
-                {
-                    //Arrange
-                    $name = "Paddys";
-                    $location = "462 Over There Way";
-                    $link = "www.paddyspub.com";
-                    $test_pub = new Pub($name, $location, $link);
+        function test_setLink()
+        {
+            //Arrange
+            $name = "Paddys";
+            $location = "462 Over There Way";
+            $link = "www.paddyspub.com";
+            $test_pub = new Pub($name, $location, $link);
 
-                    $new_link = "www.thebar.com";
+            $new_link = "www.thebar.com";
 
-                    //Act
-                    $test_pub->setLink($new_link);
-                    $result = $test_pub->getLink();
+            //Act
+            $test_pub->setLink($new_link);
+            $result = $test_pub->getLink();
 
-                    //Assert
-                    $this->assertEquals($new_link, $result);
-                }
-}
+            //Assert
+            $this->assertEquals($new_link, $result);
+        }
+
+        function test_getId()
+        {
+            //Arrange
+            $name = "Paddys";
+            $location = "462 Over There Way";
+            $link = "www.paddyspub.com";
+            $id = 1;
+            $test_pub = new Pub($name, $location, $link, $id);
+
+            //Act
+            $result = $test_pub->getId();
+
+            //Assert
+            $this->assertEquals($id, $result);
+        }
+    }
 
 ?>
