6c74c638118d4fa4bb48515feb725818c10d3b45
Phillip Shannon & Austin Blanchard <student@epicodus.com>
2015-08-31 15:10:04 -0700

    Add save, getAll, deleteAll methods to Beer class.

--- tests/BeerTest.php
+++ tests/BeerTest.php
@@ -54,18 +54,17 @@
             $test_beer->save();
 
             $id = null;
-            $name = "Hip Hops";
-            $type = "Pale Ale";
-            $abv = 3.2;
-            $ibu = 4;
-            $region = "South Central LA";
-            $brewery_id = 2;
-            $test_beer2 = new Beer($id, $name, $type, $abv, $ibu, $region, $brewery_id);
+            $name2 = "Hip Hops";
+            $type2 = "Pale Ale";
+            $abv2 = 3.2;
+            $ibu2 = 4;
+            $region2 = "South Central LA";
+            $brewery_id2 = 2;
+            $test_beer2 = new Beer($id, $name2, $type2, $abv2, $ibu2, $region2, $brewery_id2);
             $test_beer2->save();
 
             //Act
             $result = Beer::getAll();
-
 
             //Assert
             $this->assertEquals([$test_beer, $test_beer2], $result);
