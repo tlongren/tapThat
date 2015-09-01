2755a3db97753efa6bca7d079176f74d3d6eb1b2
Austin Blanchard & Phillip Shannon <student@epicodus.com>
2015-09-01 15:17:56 -0700

    Add pub.html.twig

--- app/app.php
+++ app/app.php
@@ -26,6 +26,25 @@
         return $app['twig']->render('index.html.twig');
     });
 
+    $app->get('/pub_login', function() use ($app) {
+        $all_pubs = Pub::getAll();
+        return $app['twig']->render('pub.html.twig', array('all_pubs' => $all_pubs));
+    });
+
+    $app->post('/pub_login', function() use ($app) {
+        $name = $_POST['name'];
+        $location = $_POST['location'];
+        $link = $_POST['link'];
+        $new_pub = new Pub($name, $location, $link);
+        $new_pub->save();
+        return $app['twig']->render('pub.html.twig', array('all_pubs' => Pub::getAll()));
+    });
+
+    $app->delete('/pub_login', function() use ($app) {
+        Pub::deleteAll();
+        return $app['twig']->render('pub.html.twig', array('all_pubs' => Pub::getAll()));
+    });
+
     return $app;
 
 ?>
