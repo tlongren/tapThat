17c52c452b62d8af4e3eb8d06ed07b03b82f2339
Austin Blanchard & Phillip Shannon <student@epicodus.com>
2015-09-01 17:00:05 -0700

    Add search although it is broken

--- app/app.php
+++ app/app.php
@@ -45,6 +45,35 @@
         return $app['twig']->render('pub.html.twig', array('all_pubs' => Pub::getAll()));
     });
 
+    $app->get('pub/{id}', function($id) use ($app) {
+        $pub = Pub::find($id);
+        $id = null;
+        $name = "Lip Blaster";
+        $type = "IPA";
+        $abv = 4.2;
+        $ibu = 10;
+        $region = "Pacific Northwest";
+        $brewery_id = 1;
+        $test_beer = new Beer($id, $name, $type, $abv, $ibu, $region, $brewery_id);
+        $test_beer->save();
+
+        $new_name = "Hip Hops";
+        $new_type = "Pale Ale";
+        $new_abv = 3.2;
+        $new_ibu = 4;
+        $new_region = "South Central LA";
+        $new_brewery_id = 2;
+        $test_beer2 = new Beer($id, $name, $type, $abv, $ibu, $region, $brewery_id);
+        $test_beer2->save();
+
+        $all_beers = Beer::getAll();
+        $all_beer_names = "";
+        foreach ($all_beers as $beer) {
+            $all_beer_names = $all_beer_names . "{ label: '" . $beer->getName() . "' },";
+        }
+        return $app['twig']->render('pub_profile.html.twig', array('pub' => $pub, 'beers' => $pub->getBeers(), 'all_beer_names' => $all_beer_names));
+    });
+
     return $app;
 
 ?>
