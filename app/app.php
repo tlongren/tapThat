338ea4f6a7f3e2dae4acadb52c36a439f1c3cea3
Alexandra Brown & Deron Johnson <student@epicodus.com>
2015-09-03 14:43:17 -0700

    Add test and method for getBrews.

--- app/app.php
+++ app/app.php
@@ -150,7 +150,7 @@
         $link = $_POST['link'];
         $new_pub = new Pub($name, $location, $link);
         $new_pub->save();
-        return $app['twig']->render('pub.html.twig', array('all_pubs' => Pub::getAll()));
+        return $app['twig']->render('pub.html.twig', array('pubs' => Pub::getAll()));
     });
 
     //deletes all the pubs
