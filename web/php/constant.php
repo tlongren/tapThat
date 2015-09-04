aece9d4eefc14f19a72329ecae14e581f020b0b2
Deron Johnson <johdero@gmail.com>
2015-09-03 22:51:11 -0700

    Change jumbotron opacity

--- web/php/constant.php
+++ web/php/constant.php
@@ -1,6 +1,6 @@
 <?php
 
-    define('DB_SERVER', "localhost:3306");
+    define('DB_SERVER', "localhost");
     define('DB_USER', "root");
     define('DB_PASSWORD', "root");
     define('DB_DATABASE', "tap_that");
